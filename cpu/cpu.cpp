c25811c0d80ef257c9aabc617a51793e5ba1f7f2
Peter Kis <md2z34@gmail.com>
2017-10-09 19:02:44 +0200

    trans_F_2x2_3x3/1

--- cpu/cpu.cpp
+++ cpu/cpu.cpp
@@ -6,16 +6,48 @@
 #include <vector>
 using namespace std;
 
+inline int mat2vec_idx(int row, int col, int row_n)
+{
+	return (row*row_n + col);
+}
+
+inline void vec2mat_idx(int N, int row_n, int *row, int *col)
+{
+	*row = N / row_n;
+	*col = N % row_n;
+}
+
+void trans_F_2x2_3x3(float **Fw, // Output 4x4
+					 float **F) // Input 3x3
+{
+	// Minimal version only
+	float T0[4][3];
+	float T1[4][4];
+
+
+}
+
 int main()
 {
+	int r = 3;
+	int c = 4;
 	vector<int> vec;
-	vec.push_back(1);
-	vec.push_back(15);
-	cout << "Hello" << endl;
-	for (size_t i = 0; i < vec.size(); i++)
-	{
-		cout << "vec[" << i << "]=" << vec.at(i) << endl;
+	vec.clear();
+	vec.reserve(r*c);
+	int k = 0;
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < c; ++j) {
+			vec.push_back(k++);
+		}
 	}
+
+	cout << "Hello" << endl;
+	int rk = 2;
+	int ck = 3;
+	int i = mat2vec_idx(rk,ck,c);
+	i = 4;
+	vec2mat_idx(i, c, &rk, &ck);
+	cout << "vec[" << rk << "," << ck << "]=" << vec.at(i) << endl;
 	return 0;
 }
 
